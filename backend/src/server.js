baeead359df3b1f92a30416ce53f950e88b5668c
Rohit Kumar <rohit3302ku@gmail.com>
2026-04-02 13:21:08 +0530

    db-setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import path from "path"
 
 import { ENV } from "./lib/env.js"
+import { connect } from "http2"
 
 const app = express()
 
@@ -22,6 +23,17 @@ if (ENV.NODE_ENV === "production") {
   app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
   })
+}
+
+const startServer = async () => {
+  try {
+    await connectDB()
+    app.listen(ENV.PORT, () =>
+      console.log("Server is running on port:", ENV.PORT),
+    )
+  } catch (error) {
+    console.error("💣 Error starting the server", error)
+  }
 }
 
 app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT))
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { ClerkProvider } from "@clerk/clerk-react"
+import "./index.css"
+import App from "./App.jsx"
 
-createRoot(document.getElementById('root')).render(
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+// Import your PUblishable Key
+// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_EYE
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Clerk Publishable Key")
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )
